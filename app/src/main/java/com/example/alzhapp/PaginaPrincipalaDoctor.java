01d20fef2309b6b18c17bf4d3c79733eab2de23c
Stefania <stefania.steaburdea@gmail.com>
2021-11-10 21:39:32 +0200

    preluare pacienti in functie de mail doctor

--- app/src/main/java/com/example/alzhapp/PaginaPrincipalaDoctor.java
+++ app/src/main/java/com/example/alzhapp/PaginaPrincipalaDoctor.java
@@ -2,6 +2,8 @@ package com.example.alzhapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,12 +21,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class PaginaPrincipalaDoctor extends AppCompatActivity {
 
     private FirebaseUser user;
     private DatabaseReference db;
     private String userId;
     private ImageButton logout;
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<Users> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +63,36 @@ public class PaginaPrincipalaDoctor extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(PaginaPrincipalaDoctor.this,"Eroare",Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView = findViewById(R.id.myBoxPacienti);
+        database = FirebaseDatabase.getInstance().getReference().child("users");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user=FirebaseAuth.getInstance().getCurrentUser();
+                String m=user.getEmail();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+
+                    Users u = dataSnapshot.getValue(Users.class);
+                    if(u.getTipUtilizator().equals("pacient"))
+                        if(u.getMailDoctor().equals(m))
+                           list.add(u);
+                }
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
