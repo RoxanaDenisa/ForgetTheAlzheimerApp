0599750d031261a9beba02f1f4166c280b974a90
RoxanaDenisa <virlan_roxana99@yahoo.com>
2021-11-13 20:43:35 +0200

    Creare pagina detalii pacienti

---
+++ app/src/main/java/com/example/alzhapp/DetaliiPacienti.java
@@ -0,0 +1,14 @@
+package com.example.alzhapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetaliiPacienti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalii_pacienti);
+    }
+}
