3e9622cf7e57501be048b61e08860e2fee692fb4
RoxanaDenisa <virlan_roxana99@yahoo.com>
2021-11-15 19:59:37 +0200

    Legare pagina medicamentatie cu pagina adaugare medicament

--- app/src/main/java/com/example/alzhapp/Medicamentatie.java
+++ app/src/main/java/com/example/alzhapp/Medicamentatie.java
@@ -2,13 +2,28 @@ package com.example.alzhapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Medicamentatie extends AppCompatActivity {
+    private ImageButton adaugare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medicamentatie);
+
+        adaugare=findViewById(R.id.adaugare);
+        adaugare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Medicamentatie.this,AdaugareMedicament.class));
+            }
+        });
     }
 }
